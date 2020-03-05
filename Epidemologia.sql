82906171ef972dc1ae92644ef29d399ecb1c12f9
Matheus Feitosa <matheusfeitosaufrj@gmail.com>
2020-03-05 15:55:52 -0300

    terminei

--- Epidemologia.sql
+++ Epidemologia.sql
@@ -48,6 +48,8 @@ CartaoSUS INTEGER,
 cpf INTEGER,
 nomepaciente VARCHAR(255) NOT NULL,
 sexo VARCHAR(1),
+gestante INTEGER,
+trimestre_gestacional INTEGER,
 data_nascimento DATE NOT NULL,
 idadeanos INTEGER,
 nomemae VARCHAR(255) NOT NULL,
@@ -56,8 +58,121 @@ paisresidencia VARCHAR(255),
 unidadefederal VARCHAR(2),
 endereco VARCHAR(255),
 cep INTEGER,
-CONSTRAINT paciente_pk PRIMARY KEY (nomepaciente, data_nascimento, nomemae)
+PRIMARY KEY (nomepaciente, data_nascimento, nomemae)
 );
+
+CREATE TABLE dadosdecasos (
+id_dadosdecasos INTEGER PRIMARY KEY AUTOINCREMENT,
+dadosdecasos_nomepaciente VARCHAR(255) NOT NULL,
+dadosdecasos_data_nascimento DATE NOT NULL,
+dadosdecasos_nomemae VARCHAR(255) NOT NULL,
+data_primeirossintomas DATE,
+sintomas_febre INTEGER,
+sintomas_tosse INTEGER,
+sintomas_dor_de_garganta INTEGER,
+sintomas_dificuldade_de_respirar INTEGER,
+sintomas_mialgia INTEGER,
+sintomas_diarreia INTEGER,
+sintomas_nausea INTEGER,
+sintomas_cefaleia INTEGER,
+sintomas_coriza INTEGER,
+sintomas_irritabilidade INTEGER,
+sintomas_adinamia INTEGER,
+sintomas_producao_de_escarro INTEGER,
+sintomas_calafrios INTEGER,
+sintomas_congestao_nasal INTEGER,
+sintomas_congestao_conjuntiva INTEGER,
+sintomas_dificuldade_para_deglutir INTEGER,
+sintomas_manchas_vermelhas_pelo_corpo INTEGER,
+sintomas_ganglios_linfaticos_aumentados INTEGER,
+sintomas_batimento_das_asas_nasais INTEGER,
+sintomas_outros INTEGER,
+outrossintomas VARCHAR(255),
+sintomas_clinicos_febre INTEGER,
+temperatura_aferida INTEGER,
+sintomas_clinicos_exsudato_faringeo INTEGER,
+sintomas_clinicos_convulsao INTEGER,
+sintomas_clinicos_conjuntivite INTEGER,
+sintomas_clinicos_coma INTEGER,
+sintomas_clinicos_dispneia INTEGER,
+sintomas_clinicos_alteracao_de_ausculta_pulmonar INTEGER,
+sintomas_clinicos_alteracao_na_radiologia_de_torax INTEGER,
+sintomas_clinicos_outros INTEGER,
+outrossintomasclinicos VARCHAR(255),
+morbidades_previas_doenca_cardiovascular INTEGER,
+morbidades_previas_diabetes INTEGER,
+morbidades_previas_doenca_hepatica INTEGER,
+morbidades_previas_doenca_neurologica_cronica INTEGER,
+morbidades_previas_imunodeficiencia INTEGER,
+morbidades_previas_infeccao_pelo_hiv INTEGER,
+morbidades_previas_doenca_renal INTEGER,
+morbidades_previas_doenca_pulmonar_cronica INTEGER,
+morbidades_previas_neoplasia INTEGER,
+paciente_foi_hospitalizado INTEGER,
+nome_do_hospital_de_internacao VARCHAR(255),
+data_da_internacao_hospitalar DATE,
+data_da_alta_hospitalar DATE,
+data_do_isolamento DATE,
+paciente_foi_submetido_a_ventilacao_mecanica INTEGER,
+setuacao_de_saude_do_paciente INTEGER,
+foi_realizado_coleta_de_amostra_do_paciente INTEGER,
+FOREIGN KEY (dadosdecasos_nomepaciente, dadosdecasos_data_nascimento, dadosdecasos_nomemae) REFERENCES paciente(nomepaciente, data_nascimento, nomemae)
+);
+
+
+CREATE TABLE viagens (
+id_viagens INTEGER PRIMARY KEY AUTOINCREMENT,
+viagens_nomepaciente VARCHAR(255) NOT NULL,
+viagens_data_nascimento DATE NOT NULL,
+viagens_nomemae VARCHAR(255) NOT NULL,
+paciente_tem_historico_de_viagem_para_fora_do_brasil_14_dias INTEGER,
+foi_para_wuhan_na_china INTEGER,
+data_da_viagem_de_ida_para_wuhan DATE,
+data_da_viagem_de_volta_para_wuhan DATE,
+foi_para_outro_local_de_transmissao INTEGER,
+outro_local_de_transmissao_descrever VARCHAR(255),
+data_da_viagem_de_ida_para_outro_local_transmissao DATE,
+data_da_viagem_de_volta_para_outro_local_transmissao DATE,
+descrito_do_historico_de_deslocamento_nos_14_dias VARCHAR(255),
+data_da_chegada_no_brasil DATE,
+o_paciente_teve_contato_proximo_com_uma_pessoa_que_seja_caso_suspeito INTEGER,
+se_teve_contato_com_outro_caso_suspeito_unidade_de_saude INTEGER,
+se_teve_contato_com_outro_caso_suspeito_domicilio INTEGER,
+se_teve_contato_com_outro_caso_suspeito_local_de_trabalho INTEGER,
+se_teve_contato_com_outro_caso_suspeito_desconhecido INTEGER,
+se_teve_contato_com_outro_caso_suspeito_outro INTEGER,
+se_teve_contato_com_outro_caso_suspeito_especificar VARCHAR(255),
+o_paciente_teve_contato_proximo_com_uma_pessoa_que_seja_caso_confirmado INTEGER,
+se_teve_contato_com_outro_caso_confirmado_unidade_de_saude INTEGER,
+se_teve_contato_com_outro_caso_confirmado_domicilio INTEGER,
+se_teve_contato_com_outro_caso_confirmado_local_de_trabalho INTEGER,
+se_teve_contato_com_outro_caso_confirmado_desconhecido INTEGER,
+se_teve_contato_com_outro_caso_confirmado_outro INTEGER,
+se_teve_contato_com_outro_caso_confirmado_especificar VARCHAR(255),
+nome_do_caso_fonte VARCHAR(255),
+esteve_em_alguma_unidade_de_saude_nos_14_dias_antes INTEGER,
+se_frequentou_unidade_de_saude VARCHAR(255),
+ocupacao_do_caso_suspeito INTEGER,
+ocupacao_outros_especificar VARCHAR(255),
+teve_contato_proximo_com_animais INTEGER,
+FOREIGN KEY (viagens_nomepaciente, viagens_data_nascimento, viagens_nomemae) REFERENCES paciente(nomepaciente, data_nascimento, nomemae)
+);
+
+CREATE TABLE unidadenotificadora (
+id_unidadenotificadora INTEGER PRIMARY KEY AUTOINCREMENT,
+unidadenotificadora_nomepaciente VARCHAR(255) NOT NULL,
+unidadenotificadora_data_nascimento DATE NOT NULL,
+unidadenotificadora_nomemae VARCHAR(255) NOT NULL,
+origem_da_notificacao VARCHAR(255),
+estado_de_notificacao VARCHAR(2),
+municipio_de_notificacao VARCHAR(255),
+nome_do_notificador VARCHAR(255),
+profissao_ou_ocupacao VARCHAR(255),
+telefone_de_contato_do_notificador VARCHAR(255),
+email_do_notificador VARCHAR(255),
+FOREIGN KEY (unidadenotificadora_nomepaciente, unidadenotificadora_data_nascimento, unidadenotificadora_nomemae) REFERENCES paciente(nomepaciente, data_nascimento, nomemae)
+);
+
 
 INSERT INTO transmissores
 (nome_transmissor)
@@ -109,13 +224,6 @@ INSERT INTO transmite
 VALUES
 ('2','4');
 
-INSERT INTO paciente
-(data_notificacao, nomepaciente, data_nascimento, nomemae)
-VALUES
-('2020-02-24 13:54', 'Manolin da Silva Chavier','1995-10-02','Mae do Manolin da Silva Chavier');
-
-
-
 INSERT INTO usuario
 (nome_usuario,usuario,senha,email,datanasc,adm)
 VALUES
@@ -136,12 +244,35 @@ INSERT INTO denuncia
 VALUES
 ('Rua do Manolin 123',"0",'4','1','2020-01-01','5.23','3.21');
 
+INSERT INTO paciente
+(data_notificacao, nomepaciente, data_nascimento, nomemae)
+VALUES
+('2020-02-24 13:54', 'Manolin da Silva Chavier','1995-10-02','Mae do Manolin da Silva Chavier');
+
+INSERT INTO dadosdecasos
+(dadosdecasos_nomepaciente, dadosdecasos_data_nascimento, dadosdecasos_nomemae)
+VALUES
+('Manolin da Silva Chavier','1995-10-02','Mae do Manolin da Silva Chavier');
+
+INSERT INTO viagens
+(viagens_nomepaciente, viagens_data_nascimento, viagens_nomemae)
+VALUES
+('Manolin da Silva Chavier','1995-10-02','Mae do Manolin da Silva Chavier');
+
+INSERT INTO unidadenotificadora
+(unidadenotificadora_nomepaciente, unidadenotificadora_data_nascimento, unidadenotificadora_nomemae)
+VALUES
+('Manolin da Silva Chavier','1995-10-02','Mae do Manolin da Silva Chavier');
+
 -- Retorna tudo de todos
 SELECT * FROM usuario;
 SELECT * FROM transmissores;
 SELECT * FROM doencas;
 SELECT * FROM denuncia;
 SELECT * FROM paciente;
+SELECT * FROM dadosdecasos;
+SELECT * FROM viagens;
+SELECT * FROM unidadenotificadora;
 
 -- retornar as doenças pra fazer a denuncia
 SELECT nome_transmissor FROM transmissores;
@@ -174,8 +305,4 @@ SELECT *
 FROM denuncia
 INNER JOIN doencas ON d_doenca = id_doenca
 WHERE nome_doenca = 'Dorime';
-
-
-
-
 
