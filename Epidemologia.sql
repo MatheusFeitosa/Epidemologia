09954fe789686d38057166e228adfed2bc4a0084
Matheus Feitosa <matheusfeitosaufrj@gmail.com>
2020-03-09 18:17:49 -0300

    esqueci de salvar a ultima alteracao

--- Epidemologia.sql
+++ Epidemologia.sql
@@ -41,7 +41,7 @@ FOREIGN KEY (dadosdecasos_cpf) REFERENCES paciente(cpf)
 
 CREATE TABLE viagens (
 id_viagens INTEGER PRIMARY KEY AUTOINCREMENT,
-viagens_cpf = INTEGER NOT NULL,
+viagens_cpf INTEGER NOT NULL,
 paciente_tem_historico_de_viagem_para_fora_do_brasil_14_dias INTEGER,
 foi_para_wuhan_na_china INTEGER,
 data_da_viagem_de_ida_para_wuhan DATE,
@@ -114,14 +114,19 @@ VALUES
 ('12345678910', '2020-02-24 13:54','0', '0', '24', 'Brasil', 'RJ', 'Rua do Manolin 123', '20220250');
 
 INSERT INTO dadosdecasos
-(dadosdecasos_cpf, data_primeirossintomas, temperatura_aferida, paciente_foi_hospitalizado, data_da_internacao_hospitalar, data_da_internacao_hospitalar, data_da_alta_hospitalar, data_do_isolamento, paciente_foi_submetido_a_ventilacao_mecanica, situacao_de_saude_do_paciente, foi_realizado_coleta_de_amostra_do_paciente)
+(dadosdecasos_cpf, data_primeirossintomas, temperatura_aferida, paciente_foi_hospitalizado, data_da_internacao_hospitalar, data_da_alta_hospitalar, data_do_isolamento, paciente_foi_submetido_a_ventilacao_mecanica, situacao_de_saude_do_paciente, foi_realizado_coleta_de_amostra_do_paciente)
 VALUES
-('12345678910','2020-02-20','41', '0',,,,'Cura','1','1');
+('12345678910','2020-02-20', '41', '1', '2020-02-20', '2020-02-20', '2020-02-20', '1','Cura','1');
+
+INSERT INTO dadosdecasos
+(dadosdecasos_cpf, data_primeirossintomas, temperatura_aferida, paciente_foi_hospitalizado, data_da_internacao_hospitalar, data_da_alta_hospitalar, data_do_isolamento, paciente_foi_submetido_a_ventilacao_mecanica, situacao_de_saude_do_paciente, foi_realizado_coleta_de_amostra_do_paciente)
+VALUES
+('12345678910','2020-02-20', '41', '1', '2020-02-21', '2020-02-21', '2020-02-21', '1','Cura','1');
 
 INSERT INTO viagens
 (viagens_cpf, paciente_tem_historico_de_viagem_para_fora_do_brasil_14_dias, o_paciente_teve_contato_proximo_com_uma_pessoa_que_seja_caso_suspeito, o_paciente_teve_contato_proximo_com_uma_pessoa_que_seja_caso_confirmado, esteve_em_alguma_unidade_de_saude_nos_14_dias_antes, ocupacao_do_caso_suspeito, teve_contato_proximo_com_animais)
 VALUES
-('12345678910','0','0','0', '0', 'Profissional da Saude', '1');
+('12345678910', '0', '0', '0', '0', 'Profissional da Saude', '1');
 
 INSERT INTO unidadenotificadora
 (unidadenotificadora_cpf, origem_da_notificacao, estado_de_notificacao, municipio_de_notificacao, nome_do_notificador, profissao_ou_ocupacao, telefone_de_contato_do_notificador, email_do_notificador)
@@ -145,3 +150,11 @@ SELECT * FROM dadosdecasos;
 SELECT * FROM viagens;
 SELECT * FROM unidadenotificadora;
 SELECT * FROM sintomas;
+
+-- Selects para fazer as inscriçoes
+
+-- Saber se ja esta escrito
+SELECT cpf FROM paciente where cpf = '12345678910';
+
+-- Retorna o id do dadosdecasos para cadastrar os sintomas
+SELECT max(id_dadosdecasos) FROM dadosdecasos where dadosdecasos_cpf = '12345678910';
